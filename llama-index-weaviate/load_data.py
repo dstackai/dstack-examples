14c42870cdc8311ce6d4316da0006ea4f120f6b1
peterschmidt85 <andrey.cheptsov@gmail.com>
2023-09-20 11:22:09 +0200

    1. Fixed a bug in the prompt. 2. Set the `context_window` to 1024 to avoid exceeding `max_total_tokens`. 3. Fixed the issue with the name of the Weaviate index.

--- llama-index-weaviate/load_data.py
+++ llama-index-weaviate/load_data.py
@@ -12,42 +12,48 @@ from llama_index import (
     VectorStoreIndex,
 )
 from llama_index.vector_stores import WeaviateVectorStore
+from llama_index.callbacks import CallbackManager, LlamaDebugHandler
 
-weaviate_api_key = os.getenv("WEAVIATE_API_TOKEN")
-weaviate_url = os.getenv("WEAVIATE_URL")
 
-if not weaviate_api_key or not weaviate_url:
-    exit(
-        "Make sure you've set WEAVIATE_API_TOKEN and WEAVIATE_URL environment variables"
+if __name__ == "__main__":
+    weaviate_api_key = os.getenv("WEAVIATE_API_TOKEN")
+    weaviate_url = os.getenv("WEAVIATE_URL")
+
+    if not weaviate_api_key or not weaviate_url:
+        exit(
+            "Make sure you've set WEAVIATE_API_TOKEN and WEAVIATE_URL environment variables"
+        )
+
+    auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
+
+    client = weaviate.Client(
+        url=weaviate_url,
+        auth_client_secret=auth_config,
     )
 
-auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
+    client.schema.delete_class("DstackExample")
 
-client = weaviate.Client(
-    url=weaviate_url,
-    auth_client_secret=auth_config,
-)
+    embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
 
-client.schema.delete_class("llama-index-weaviate")
+    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+    callback_manager = CallbackManager([llama_debug])
 
-embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
+    service_context = ServiceContext.from_defaults(
+        embed_model=embed_model,
+        llm=None,
+        callback_manager=callback_manager,
+    )
 
-service_context = ServiceContext.from_defaults(
-    embed_model=embed_model,
-    llm=None,
-)
+    vector_store = WeaviateVectorStore(
+        weaviate_client=client, index_name="DstackExample"
+    )
 
-vector_store = WeaviateVectorStore(
-    weaviate_client=client, index_name="llama-index-weaviate"
-)
+    storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
-storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    documents = SimpleDirectoryReader(Path(__file__).parent / "data").load_data()
 
-
-documents = SimpleDirectoryReader(Path(__file__).parent / "data").load_data()
-
-index = VectorStoreIndex.from_documents(
-    documents,
-    service_context=service_context,
-    storage_context=storage_context,
-)
+    index = VectorStoreIndex.from_documents(
+        documents,
+        service_context=service_context,
+        storage_context=storage_context,
+    )
--- llama-index-weaviate/query_data.py
+++ llama-index-weaviate/query_data.py
@@ -4,54 +4,62 @@ from langchain import HuggingFaceTextGenInference
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from llama_index import (
     LangchainEmbedding,
+    PromptHelper,
     QuestionAnswerPrompt,
     ServiceContext,
     VectorStoreIndex,
 )
 from llama_index.llm_predictor import LLMPredictor
 from llama_index.vector_stores import WeaviateVectorStore
+from llama_index.callbacks import CallbackManager, LlamaDebugHandler
 
-weaviate_api_key = os.getenv("WEAVIATE_API_TOKEN")
-weaviate_url = os.getenv("WEAVIATE_URL")
-tgi_endpoint_url = os.getenv("TGI_ENDPOINT_URL")
+if __name__ == "__main__":
+    weaviate_api_key = os.getenv("WEAVIATE_API_TOKEN")
+    weaviate_url = os.getenv("WEAVIATE_URL")
+    tgi_endpoint_url = os.getenv("TGI_ENDPOINT_URL")
 
-if not weaviate_api_key or not weaviate_url or not tgi_endpoint_url:
-    exit(
-        "Make sure you've set WEAVIATE_API_TOKEN, WEAVIATE_URL, and TGI_ENDPOINT_URL environment variables"
+    if not weaviate_api_key or not weaviate_url or not tgi_endpoint_url:
+        exit(
+            "Make sure you've set WEAVIATE_API_TOKEN, WEAVIATE_URL, and TGI_ENDPOINT_URL environment variables"
+        )
+
+    auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
+
+    client = weaviate.Client(
+        url=weaviate_url,
+        auth_client_secret=auth_config,
     )
 
-auth_config = weaviate.AuthApiKey(api_key=weaviate_api_key)
+    embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
 
-client = weaviate.Client(
-    url=weaviate_url,
-    auth_client_secret=auth_config,
-)
+    llm_predictor = LLMPredictor(
+        llm=HuggingFaceTextGenInference(
+            inference_server_url=tgi_endpoint_url,
+            max_new_tokens=512,
+            streaming=True,
+        ),
+    )
 
-embed_model = LangchainEmbedding(HuggingFaceEmbeddings())
+    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+    callback_manager = CallbackManager([llama_debug])
 
-llm_predictor = LLMPredictor(
-    llm=HuggingFaceTextGenInference(
-        inference_server_url=tgi_endpoint_url,
-        max_new_tokens=512,
-        streaming=True,
-    ),
-)
+    service_context = ServiceContext.from_defaults(
+        embed_model=embed_model,
+        llm_predictor=llm_predictor,
+        prompt_helper=PromptHelper(context_window=1024),
+        callback_manager=callback_manager,
+    )
 
-service_context = ServiceContext.from_defaults(
-    embed_model=embed_model,
-    llm_predictor=llm_predictor,
-)
+    vector_store = WeaviateVectorStore(
+        weaviate_client=client, index_name="DstackExample"
+    )
 
-vector_store = WeaviateVectorStore(
-    weaviate_client=client, index_name="llama-index-weaviate"
-)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store, service_context=service_context
+    )
 
-index = VectorStoreIndex.from_vector_store(
-    vector_store, service_context=service_context
-)
-
-prompt = QuestionAnswerPrompt(
-    """<s>[INST] <<SYS>>
+    prompt = QuestionAnswerPrompt(
+        """<s>[INST] <<SYS>>
 We have provided context information below. 
 
 {context_str}
@@ -59,14 +67,13 @@ We have provided context information below.
 Given this information, please answer the question.
 <</SYS>>
 
-[/INST]</s>
-<s>[INST]{query_str}[/INST]"""
-)
-query_engine = index.as_query_engine(
-    text_qa_template=prompt,
-    streaming=True,
-)
+{query_str} [/INST]"""
+    )
+    query_engine = index.as_query_engine(
+        text_qa_template=prompt,
+        streaming=True,
+    )
 
-response = query_engine.query("What did the author do growing up?")
-response.print_response_stream()
-print(f"\n\nSources:\n\n{response.get_formatted_sources()}")
+    response = query_engine.query("What did the author do growing up?")
+    response.print_response_stream()
+    print(f"\n\nSources:\n\n{response.get_formatted_sources()}")
